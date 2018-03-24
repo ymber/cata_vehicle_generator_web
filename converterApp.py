361735ba1e4dc140071e2ff653d4871fdcb1238c
ymber <mtaylor1252dev@gmail.com>
2018-03-24 19:48:55 +0000

    initial commit

---
+++ converterApp.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, url_for, request
+import json
+
+app = Flask(__name__)
+
+def findVehicle(saveData, vehicleName):
+    for i in range(0, len(saveData)):
+        for vehicle in saveData[i]["vehicles"]:
+            if (vehicleName == vehicle["name"]):
+                return vehicle, vehicleName
+
+def writeDef(vehicle, vehicleName):
+    partsDef = []
+    for part in vehicle["parts"]:
+        partsDef.append({"x": part["mount_dx"],
+                         "y": part["mount_dy"],
+                         "part": part["id"]})
+
+    vehicleDef = {"id": vehicleName,
+                   "type": "vehicle",
+                   "name": vehicleName,
+                   "parts": partsDef}
+
+    return json.dumps(vehicleDef)
+
+@app.route("/")
+def index():
+    return render_template("index.html", teststring="Cataclysm DDA Vehicle Generator")
+
+@app.route("/out", methods=["POST"])
+def rawOut():
+    return render_template("out.html", output=writeDef(*findVehicle(json.loads(request.form["mapdata"]), request.form["vehicleName"])))
